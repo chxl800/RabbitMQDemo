98872d73fa445ca0f6ba566af3c568cd6a18acab
chenxiaolong <chenxiaolong@PC-20180323IYVO>
2019-03-08 17:38:44 +0800

    提交

---
+++ RabbitMQ/Consumer/Program.cs
@@ -0,0 +1,83 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumerTwo();
+        }
+
+        private static void ConsumerOne()
+        {
+            //消费者
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "127.0.0.1";
+            //默认端口
+            factory.Port = 5672;
+            using (IConnection conn = factory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    //在MQ上定义一个持久化队列，如果名称相同不会重复创建
+                    channel.QueueDeclare("MyRabbitMQ", true, false, false, null);
+
+                    //输入1，那如果接收一个消息，但是没有应答，则客户端不会收到下一个消息
+                    channel.BasicQos(0, 1, false);
+
+                    Console.WriteLine("Listening...");
+
+                    //在队列上定义一个消费者
+                    QueueingBasicConsumer consumer = new QueueingBasicConsumer(channel);
+                    //消费队列，并设置应答模式为程序主动应答
+                    channel.BasicConsume("MyRabbitMQ", false, consumer);
+
+                    while (true)
+                    {
+                        //阻塞函数，获取队列中的消息
+                        BasicDeliverEventArgs ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                        byte[] bytes = ea.Body;
+                        string str = Encoding.UTF8.GetString(bytes);
+
+                        Console.WriteLine("队列消息:" + str.ToString());
+                        //回复确认
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                }
+            }
+        }
+
+        private static void ConsumerTwo()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "127.0.0.1";
+            factory.Port = 5672;
+
+            var conne = factory.CreateConnection();
+            var model = conne.CreateModel();
+
+            model.QueueDeclare("RabbitMqTwo", true, false, false, null);
+            var consumer = new EventingBasicConsumer(model);
+            model.BasicConsume("RabbitMqTwo", false, consumer);
+
+            Console.WriteLine("listen...");
+
+            //接受消息
+            consumer.Received += (s, e) =>
+            {
+                var bitmage = e.Body;
+                var magess = Encoding.UTF8.GetString(bitmage);
+                Console.WriteLine($"接受消息：{magess}");
+                model.BasicAck(e.DeliveryTag, false);
+                //model.BasicConsume("RabbitMqTwo", true, consumer);
+            };
+
+        }
+    }
+}
---
+++ RabbitMQ/Producer/Program.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProducerTwo();
+        }
+
+        private static void ProducerOne()
+        {
+            Console.WriteLine("Writing...");
+            //生产者
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "127.0.0.1";
+            //默认端口
+            factory.Port = 5672;
+            using (IConnection conn = factory.CreateConnection())
+            {
+                using (IModel channel = conn.CreateModel())
+                {
+                    //channel.ExchangeDeclare("",ExchangeType.Direct);
+                    //在MQ上定义一个持久化队列，如果名称相同不会重复创建
+                    channel.QueueDeclare("MyRabbitMQ", true, false, false, null);
+                    while (true)
+                    {
+                        string message = string.Format("Message_{0}", Console.ReadLine());
+                        byte[] buffer = Encoding.UTF8.GetBytes(message);
+                        IBasicProperties properties = channel.CreateBasicProperties();
+                        properties.DeliveryMode = 2;
+                        channel.BasicPublish("", "MyRabbitMQ", properties, buffer);
+                        Console.WriteLine("消息发送成功：" + message);
+                    }
+                }
+            }
+        }
+
+        private static void ProducerTwo()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "127.0.0.1";
+            factory.Port = 5672;
+
+            var conne = factory.CreateConnection();
+            var model = conne.CreateModel();
+            model.QueueDeclare("RabbitMqTwo", true, false, false, null);
+
+            Console.WriteLine("write....");
+            //发消息
+            while (true)
+            {
+                string magess = Console.ReadLine();
+                var bitmage = Encoding.UTF8.GetBytes(magess);
+                var proper = model.CreateBasicProperties();
+                proper.DeliveryMode = 2;
+
+                model.BasicPublish("", "RabbitMqTwo", proper, bitmage);
+                Console.WriteLine($"发送消息：{magess}");
+            }
+
+        }
+    }
+}
